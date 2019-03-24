07d2a46bbfdf19bea03f0308d61582d7b3fb9a0f
Evgeny Lutsky <evlutsky@microsoft.com>
2019-03-24 20:57:57 +0200

    added json vs text output formats

--- HW/App/Controllers/HomeworkController.cs
+++ HW/App/Controllers/HomeworkController.cs
@@ -6,21 +6,45 @@ using System.Threading.Tasks;
 
 namespace HW.App.Controllers
 {
+    using System.Net.Http;
+    using System.Text;
+
     using HW.Infrastructure;
     using HW.Infrastructure.Entities;
 
-    [Route("api/[controller]")]
     [ApiController]
     public class HomeworkController : ControllerBase
     {
         private HomeworkProvider _homeworkProvider = new HomeworkProvider();
         // GET api/values
+        [Route("api/[controller]")]
         [HttpGet]
-        public ActionResult<IEnumerable<CourseHomework>> Get()
+        public ActionResult<IEnumerable<CourseHomework>> Get([FromQuery] bool json = false)
         {
             var homeworks = _homeworkProvider.GetHomeWork();
+            if (json)
+            {
 
-            return homeworks;
+                return homeworks;
+            }
+
+            var responseText = new StringBuilder();
+            foreach (var homework in homeworks)
+            {
+                responseText.AppendLine(homework.CourseName);
+                foreach (var lesson in homework.Lessons)
+                {
+                    responseText.AppendLine("------------------");
+                    responseText.AppendLine($"{lesson.Date}");
+                    responseText.AppendLine($"Topic: {lesson.Topic}");
+                    responseText.AppendLine($"HW: {lesson.Homework}");
+                }
+                responseText.AppendLine("==========================");
+            }
+
+
+            return Content(responseText.ToString());
+
         }
 
     }
